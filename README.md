1de8c5c7f94163645fc98ed611f35adcefae3132
boland <boland@us.ibm.com>
2019-10-14 22:55:00 -0400

    adding basic instructions on how to get this working and an example

---
+++ README.md
@@ -0,0 +1,35 @@
+# Hello Quantum Arcade Edition
+
+This repo contains all of the html5 web files to run Hello Quanutm Arcade Edition on the web.
+
+
+I have included the basic node server files for being able to run this on a server.  This is neccessary because it will not run unless its served.
+
+To setup make sure you have `node` installed.
+
+
+Then run
+
+`npm install`
+
+and finally 
+
+`npm start` 
+
+to run locally.  The default is `localhost:3000`.
+
+If you only care about the actual files that are Hello Quantum then seek out the `views` folder.
+
+`cd views` and you will find a folder called HelloQuantum.  In there you will find all of the files that run Hello Quantum.
+
+To see a running example of these files, I have put it up here.
+
+[http://hello-quantum.qiskit.org](http://hello-quantum.qiskit.org)
+
+Once you get this running on the touch screen and you think it might need graphic adjustments, please let me know and I will work on getting you an updated version.
+
+If you have any issues or problems, or just want to chat, email me at boland@us.ibm.com
+
+Good Luck!
+
+greg
---
+++ server.js
@@ -0,0 +1,64 @@
+var express = require("express");
+var app = express();
+var cfenv = require("cfenv");
+var bodyParser = require('body-parser')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+/* Endpoint to greet and add a new visitor to database.
+* Send a POST request to localhost:3000/api/visitors with body
+* {
+*   "name": "Bob"
+* }
+*/
+app.post("/api/visitors", function (request, response) {
+  var userName = request.body.name;
+  var doc = { "name" : userName };
+  if(!mydb) {
+    console.log("No database.");
+    response.send(doc);
+    return;
+  }
+  insertOne[vendor](doc, response);
+});
+
+/**
+ * Endpoint to get a JSON array of all the visitors in the database
+ * REST API example:
+ * <code>
+ * GET http://localhost:3000/api/visitors
+ * </code>
+ *
+ * Response:
+ * [ "Bob", "Jane" ]
+ * @return An array of all the visitor names
+ */
+app.get("/api/visitors", function (request, response) {
+    response.json("{'names':'hello'}");
+    return;
+});
+
+// load local VCAP configuration  and service credentials
+var vcapLocal;
+try {
+  vcapLocal = require('./vcap-local.json');
+  console.log("Loaded local VCAP", vcapLocal);
+} catch (e) { }
+
+const appEnvOpts = vcapLocal ? { vcap: vcapLocal} : {}
+
+const appEnv = cfenv.getAppEnv(appEnvOpts);
+
+//serve static file (index.html, images, css)
+app.use(express.static(__dirname + '/views/HelloQuantum'));
+
+
+
+var port = process.env.PORT || 3000
+app.listen(port, function() {
+    console.log("To view your app, open this link in your browser: http://localhost:" + port);
+});
